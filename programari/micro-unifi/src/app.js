4497a765dc0a240daacfb49c071acc5e2ef17b56
sergivb01 <sergivb01@gmail.com>
2018-06-21 22:55:54 +0200

    Move code to "programari" folder

--- programari/micro-unifi/src/app.js
+++ programari/micro-unifi/src/app.js
@@ -1,14 +1,15 @@
-/*let unifi = require('node-unifiapi')
-let unifi = UnifiAPI({
+let unifi = require('node-unifiapi')({
 	baseUrl: 'https://127.0.0.1:8443', // The URL of the Unifi Controller
 	username: 'ubnt',
 	password: 'ubnt',
 	debug: false,
 	debugNet: false
-});*/
+});
 
 let run = () => {
-
+	unifi.stat_sites().then((data) => {
+		console.log(JSON.stringify(data))
+	})
 }
 
 setInterval(run, 1000);
