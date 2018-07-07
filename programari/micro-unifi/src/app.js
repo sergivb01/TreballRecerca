1447fb33dbd54f6004e71b90836ffa9bd3e29039
sergivb01 <sergivb01@gmail.com>
2018-07-07 14:01:41 +0200

    API improve a lot

--- programari/micro-unifi/src/app.js
+++ programari/micro-unifi/src/app.js
@@ -1,22 +1,58 @@
-let unifi = require('node-unifiapi')({
-	baseUrl: 'https://192.168.1.200:8443', // The URL of the Unifi Controller
-	username: 'root',
-	password: 'sergivb01_',
-	debug: false,
-	debugNet: false
-});
+const express = require('express'),
+	log = require('consola'),
+	app = express(),
+	morgan = require('morgan'),
+	fs = require('fs'),
+	path = require('path'),
+	accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' }),
+	mongoose = require('mongoose')
 
-let run = () => {
-	/*unifi.list_clients().then((data) => {
-		let dat = JSON.stringify(data)
-		console.log(`Data is ${dat}`)
-	})*/
+/*mongoose.connect('mongodb://localhost/tr-sergi').then(() => {
+	log.info(`Successfuly connected to MongoDB`)
+}).catch((err) => {
+	log.error(err)
+})*/
 
-	unifi.stat_allusers().then((data) => {
-		let dat = JSON.stringify(data)
-		console.log(`Data is ${dat}`)
+morgan.token('remote-addr', (req) => { //Running under reverse proxy
+	return req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress
+})
+
+app.use(
+	require('express-session')({
+		cookie: {
+			maxAge: 1800000,
+			httpOnly: true
+		},
+		rolling: true,
+		resave: true,
+		saveUninitialized: true,
+		secret: 'mashitlol'
+	}),
+	morgan('combined', { //Log saver
+		stream: accessLogStream
+	}),
+	morgan('dev'), //Log into console
+)
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/api', require('./routes/api'))
+
+//404 handler
+/*app.use((req, res, next) => {
+	res.json({
+		"error": true,
+		"message": "Invalid API usage or 404."
 	})
-}
+	next()
+})*/
 
-run()
-//setInterval(run, 1000);
+// error handler
+app.use((err, req, res, next) => {
+	log.error(err)
+	res.json({
+		"error": true,
+		"message": err
+	})
+})
+
+module.exports = app
