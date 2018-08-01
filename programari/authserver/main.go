2d7e2855debc814f260c7e1c3b2bd57198575dc9
sergivb01 <sergivb01@gmail.com>
2018-08-01 19:57:22 +0200

    Started new project structure

---
+++ programari/authserver/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+
+	"github.com/dchest/uniuri"
+	"github.com/gorilla/mux"
+)
+
+var googleOauthConfig = &oauth2.Config{
+	RedirectURL: "http://127.0.0.1:3000/callback",
+	ClientID:/*os.Getenv("GOOGLE_CLIENT_ID")*/ "97943723947-ij93jbog1b0p0v7qmaagk52etf23f9pu.apps.googleusercontent.com",
+	ClientSecret:/*os.Getenv("GOOGLE_CLIENT_SECRET")*/ "_d3xl1zEp-IMY7Evo5Hz_hYs",
+	Scopes: []string{
+		"https://www.googleapis.com/auth/userinfo.profile",
+		"https://www.googleapis.com/auth/userinfo.email"},
+	Endpoint: google.Endpoint,
+}
+
+type GoogleUser struct {
+	ID            string `json:"id"`
+	Email         string `json:"email"`
+	VerifiedEmail bool   `json:"verified_email"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Link          string `json:"link"`
+	Picture       string `json:"picture"`
+	Gender        string `json:"gender"`
+	Locale        string `json:"locale"`
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", indexHandler)
+	router.HandleFunc("/login", loginHandler)
+	router.HandleFunc("/callback", callbackHandler)
+
+	http.ListenAndServe(":3000", router)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<a href='/login'>Log in with Google</a>")
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	oauthStateString := uniuri.New()
+	url := googleOauthConfig.AuthCodeURL(oauthStateString)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	code := r.FormValue("code")
+	token, err := googleOauthConfig.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		fmt.Fprintf(w, "Code exchange failed with error %s\n", err.Error())
+		return
+	}
+
+	if !token.Valid() {
+		fmt.Fprintln(w, "Retreived invalid token")
+		return
+	}
+
+	fmt.Fprintln(w, token.AccessToken)
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		log.Printf("Error getting user from token %s\n", err.Error())
+	}
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+
+	var user *GoogleUser
+	err = json.Unmarshal(contents, &user)
+	if err != nil {
+		log.Printf("Error unmarshaling Google user %s\n", err.Error())
+		return
+	}
+
+	if !strings.HasSuffix(user.Email, "@ieslabisbal.cat") {
+		fmt.Fprint(w, "Invalid email. Email must be from @ieslabisbal.cat")
+		return
+	}
+
+	fmt.Fprintf(w, "Email: %s\nName: %s\nImage link: %s\n", user.Email, user.Name, user.Picture)
+
+}
