d32d669247e4b12b4941bc67f88287f8eb73ceb2
sergivb01 <sergivb01@gmail.com>
2018-08-30 03:15:57 +0200

    graphs...

--- programari/hotspot-manager/static/assets/js/statistics.js
+++ programari/hotspot-manager/static/assets/js/statistics.js
@@ -2,7 +2,9 @@ let COLORS;
 
 document.addEventListener('DOMContentLoaded', () => {
 	updateColors()
-	initCharts()
+	createQueriesChart()
+	createQueriesTypesChart()
+	createAPsChart()
 })
 
 let
@@ -11,23 +13,129 @@ let
 			COLORS = data
 		})
 	},
-	initCharts = () => {
-		getJSON("/assets/data/charts.json", (err, charts) => {
-			if (err) console.error(err)
+	createQueriesChart = () => {
+		let details = {
+			"domains_over_time": "Domains",
+			"ads_over_time": "Ads"
+		}
 
-			charts.forEach(chart => {
-				fetch(chart.address)
-					.then((res) => {
-						return res.json()
-					})
-					.then((json) => {
-						//testChart(json)
-						createChart(chart, json)
-					})
+		fetch("http://127.0.0.1/api/pihole/overtime")
+			.then((res) => {
+				return res.json()
 			})
-		})
+			.then((json) => {
+				let data = [], c = 0
+				Object.keys(details).forEach(entry => {
+					if (c >= COLORS.length) c = 0 //Prevent getting out of colors, starting again
+
+					data.push({
+						label: details[entry],
+						fill: false,
+						backgroundColor: COLORS[c],
+						borderColor: COLORS[c],
+						data: Object.values(json[entry])
+					})
+
+					c++
+				})
+
+				let labels = Object.keys(json[Object.keys(details)[0]]).map(unixtimestamp => {
+					let date = moment.unix(unixtimestamp).toDate()
+					return date
+				})
+				createNewChart("domainsAds", "line", "Domains and Ads over time", labels, data, {
+					"xAxes": [
+						{
+							"type": "time",
+							"time": {
+								"format": "timeFormat",
+								"unitStepSize": 1,
+								"tooltipFormat": "ll HH:mm"
+							},
+							"scaleLabel": {
+								"display": true,
+								"labelString": "Date"
+							}
+						}
+					],
+					"yAxes": [
+						{
+							"scaleLabel": {
+								"display": true,
+								"labelString": "value"
+							}
+						}
+					]
+				})
+			})
 	},
-	createChart = (chart, json) => {
+	createQueriesTypesChart = () => {
+		let details = {
+			"querytypes": "Query Types"
+		}
+
+		fetch("http://127.0.0.1/api/pihole/querytypes")
+			.then((res) => {
+				return res.json()
+			})
+			.then((json) => {
+				let data = [], c = 0, map = json[Object.keys(details)[0]]
+				delete map["ANY"]
+
+				data.push({
+					label: "test",
+					fill: false,
+					backgroundColor: COLORS.slice(0, Object.keys(map).length),
+					borderColor: COLORS.slice(0, Object.keys(map).length),
+					data: Object.values(map)
+				})
+
+				let labels = Object.keys(json[Object.keys(details)[0]])
+
+				createNewChart("queryResponses", "doughnut", "Query Types", labels, data, [])
+			})
+	},
+	createAPsChart = () => {
+		let details = {
+			"data": "Usage"
+		}
+
+		fetch("http://127.0.0.1/api/unifi")
+			.then((res) => {
+				return res.json()
+			})
+			.then((json) => {
+				let data = [], c = 0, labels = []
+
+				let map = json[Object.keys(details)[0]]
+
+
+				createNewChart("apUsage", "line", "APs usage", [], [], {
+					"xAxes": [
+						{
+							"type": "time",
+							"time": {
+								"format": "timeFormat",
+								"tooltipFormat": "ll HH:mm"
+							},
+							"scaleLabel": {
+								"display": true,
+								"labelString": "Date"
+							}
+						}
+					],
+					"yAxes": [
+						{
+							"scaleLabel": {
+								"display": true,
+								"labelString": "value"
+							}
+						}
+					]
+				})
+			})
+	},
+	/*createChart = (chart, json) => {
 		let data = []
 
 		if (Object.keys(chart.data).length > 1) {
@@ -71,7 +179,7 @@ let
 		}
 
 		createNewChart(chart.element, chart.type, chart.title, labels, data, chart.scales)
-	},
+	},*/
 
 
 	createNewChart = (element, type, title, labelList, data, scalesList) => {
